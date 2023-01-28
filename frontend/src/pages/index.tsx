a0c82cae91389b0d2390ccabdf9c7a27b503793f
joevtap <joelvitortorres@gmail.com>
2023-01-28 18:27:39 -0300

    chore: Remove unused imports

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-white flex justify-center items-center">
