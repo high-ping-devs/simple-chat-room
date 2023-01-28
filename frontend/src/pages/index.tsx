874ac65fd6399b7c0f45dcfae827f03024622186
João Lucas Lopes Dias <joaolucaslopesd@gmail.com>
2023-01-28 18:24:52 -0300

    chore: Remove Home.module.css import

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,14 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-white flex justify-center items-center">
-      <h1 className='text-5xl text-slate-800 font-bold'>Setup frontend</h1>
+      <h1 className="text-5xl text-slate-800 font-bold">Setup frontend</h1>
     </div>
-  )
+  );
 }
