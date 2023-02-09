379107b41008a693f114eeb05cfe166906b3c8f3
João Lucas Lopes Dias <joaolucaslopesd@gmail.com>
2023-02-09 14:09:04 -0300

    feat: Refatorar Formik

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,27 @@
+import SubmitButton from "@/components/SubmitButton";
+import { Form, Formik, useField, FieldHookConfig } from "formik";
 import Link from "next/link";
 import LoginInputs from "../components/LoginInputs";
+import * as Yup from "yup";
+
+const MyTextInput = (props: any) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input>. We can use field meta to show an error
+  // message if the field is invalid and it has been touched (i.e. visited)
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <input
+        className="w-full bg-neutral-800 h-16 rounded-[4px] text-neutral-300 text-base pl-4 placeholder:text-neutral-300 placeholder:text-base"
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error ? (
+        <span className="text-red-600">{meta.error}</span>
+      ) : null}
+    </>
+  );
+};
 
 export default function Login() {
   return (
@@ -11,7 +33,39 @@ export default function Login() {
             Para acessar o chat, entre com sua conta
           </p>
         </div>
-        <LoginInputs />
+
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          validationSchema={Yup.object({
+            firstName: Yup.string()
+              .max(15, "Must be 15 characters or less")
+              .required("Required"),
+            lastName: Yup.string()
+              .max(20, "Must be 20 characters or less")
+              .required("Required"),
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Required"),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {(formik) => (
+            <Form className="flex flex-col gap-4">
+              <MyTextInput name="email" type="email" placeholder="E-mail" />
+              <MyTextInput name="senha" type="password" placeholder="Senha" />
+              <SubmitButton id={"button"} name={"button"} label={"Entrar"} />
+            </Form>
+          )}
+        </Formik>
+
         <div className="w-full flex justify-center text-neutral-300 text-sm pt-4">
           <p>Não possui uma conta?</p>
           <Link href="/register" className="underline decoration-solid pl-1">
